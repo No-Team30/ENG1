2b379efa7e3c077e0d277701c02d856e7eec49c5
Rebecca Wardle <mattydwardle@gmail.com>
2020-11-23 07:49:48 +0000

    Added auber to health

--- core/src/com/team30/game/game_mechanics/Auber.java
+++ core/src/com/team30/game/game_mechanics/Auber.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 
 public class Auber extends Entity {
+    private int health;
     public Auber(TiledMapTileLayer roomTiles) {
         super(new ID(EntityType.Auber), new Texture("Auber.png"), roomTiles, 1, 1);
         this.VELOCITY_CHANGE = 2f;
         this.MAX_VELOCITY *= 1.5;
+        this.health = 100;
     }
 }
